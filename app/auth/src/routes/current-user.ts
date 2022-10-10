6811a504404db9203480d6d1378de9085f94c4c1
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-10-09 19:04:56 -0500

    auth strategies and options

--- app/auth/src/routes/current-user.ts
+++ app/auth/src/routes/current-user.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  return res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
