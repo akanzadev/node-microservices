4a0b8b33ab784a18d1fb6fed4a2f1ad9a8ab7863
akanzadev <jeancarlosmamanilingan@gmail.com>
2022-12-15 10:23:29 -0500

    common module

---
+++ app/common/src/index.ts
@@ -0,0 +1,16 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 255,
+  green: 20,
+  blue: 0
+};
+
+
+console.log(color);
+
+export default color;
